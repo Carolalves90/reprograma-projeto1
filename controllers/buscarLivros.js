e952de23278f7597286da2ddb5d0d42f419200e6
carolalves <prof.carolalves90@gmail.com>
2022-04-23 17:06:50 -0300

    funçao buscar livros

--- controllers/buscarLivros.js
+++ controllers/buscarLivros.js
@@ -1,7 +1,28 @@
 const livros = require('../database')
+const read = require('readline-sync')
 
 const buscarLivros = () => {
-    console.log('Chamei minha funcao buscar livros')
+    //quando o 1 é chamado la no app entra aqui nesse codigo 
+    //e faz essa perunta no terminal
+    const opcaoInicial = read.question('Deseja buscar por categoria? S/N ').toUpperCase()
+    //quando a pessoa responde S
+    // entra no if
+    if (opcaoInicial === 'S'){
+        //mostra no console todas as categorias existentes
+        const categorias = livros.map(livro => livro.categoria)
+        console.table(categorias)
+
+        const inputCategoria = read.question('Digite uma categoria conforme tabela acima: ')
+
+        const confirmacao = read.question('Tem certeza? S/N ').toUpperCase()
+        
+        if (confirmacao === 'S'){
+            const livrosFiltrados = livros.filter(livro => livro.categoria === inputCategoria)
+            console.table(livrosFiltrados)
+        }
+
+    }
+
 }
 
 module.exports = buscarLivros
