7db3ffac8559050cb5277db3484a7df07d50d08e
carolalves <prof.carolalves90@gmail.com>
2022-04-26 17:34:17 -0300

    livros ordenados

--- controllers/listarLivrosOrdenados.js
+++ controllers/listarLivrosOrdenados.js
@@ -2,13 +2,13 @@ const livros = require('../database')
 const read = require('readline-sync')
 
 const listarLivrosOrdenados = () => {
-    const opcaoDois = read.question(`Deseja organizar os livros de forma crescente por quantidade de paginas?`)
+    const opcaoDois = read.question(`Deseja organizar os livros de forma crescente por quantidade de paginas? S/N `).toUpperCase()
     if (opcaoDois === 'S'){
         const ordenar = livros.sort(livro => livro.paginas)
         console.table(ordenar)
-                
+        
     }
-    console.log('Chamei a funcao livros Ordenados')
+
 }
 
 module.exports = listarLivrosOrdenados
