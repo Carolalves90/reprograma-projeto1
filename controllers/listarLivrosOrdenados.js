e87ee9e49725568696944a915e9eba0513ebc3c7
carolalves <prof.carolalves90@gmail.com>
2022-04-27 13:16:38 -0300

    codigo sort funcionando por numero de paginas

--- controllers/listarLivrosOrdenados.js
+++ controllers/listarLivrosOrdenados.js
@@ -4,7 +4,7 @@ const read = require('readline-sync')
 const listarLivrosOrdenados = () => {
     const opcaoDois = read.question(`Deseja organizar os livros de forma crescente por quantidade de paginas? S/N `).toUpperCase()
     if (opcaoDois === 'S'){
-        const ordenar = livros.sort(livro => livro.paginas)
+        const ordenar = livros.sort((a,b) => a.paginas - b.paginas)
         console.table(ordenar)
         
     }
